6f69426ba14b2385fadeecf09085cdfa3922f30b
Piotr Pasciak <ppasciak@pgs-soft.com>
2015-10-09 15:36:53 +0200

    rubocop scan

--- websocket_server.rb
+++ websocket_server.rb
@@ -6,7 +6,7 @@ require 'pry-rails'
 EM.run do
   @clients = []
 
-  EM::WebSocket.start(:host => '0.0.0.0', :port => '3008') do |ws|
+  EM::WebSocket.start(host: '0.0.0.0', port: '3008') do |ws|
     ws.onopen do |handshake|
       path = handshake.path
       path[0] = ""
@@ -27,14 +27,12 @@ EM.run do
 
       puts "Received Message: #{msg}"
       if message["type"] == 1
-        @clients.each_with_index do |socket, index|
+        @clients.each do |socket|
           if message["room"] == socket.instance_variable_get(:@room)
             socket.send message["text"]
           end
-
         end
       end
     end
   end
-
 end
