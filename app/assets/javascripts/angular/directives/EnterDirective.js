86018f9b6a43794a9d80a13df337ac3e521929dc
Piotr Pasciak <ppasciak@pgs-soft.com>
2015-10-12 10:25:41 +0200

    JQuery to AngularJs refactor

---
+++ app/assets/javascripts/angular/directives/EnterDirective.js
@@ -0,0 +1,14 @@
+//=require app.js
+
+angular.module('ppApp').directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
