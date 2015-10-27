ee6e41f17989d0879eef89ea830a2384352bd396
Piotr Pasciak <ppasciak@pgs-soft.com>
2015-10-27 10:29:20 +0100

    Layout changes

--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -2,6 +2,6 @@ feature "visit home page feature", type: :feature do
   scenario "shows me home page" do
     visit root_path
 
-    expect(page).to have_content 'Poker room form'
+    expect(page).to have_content 'Poker room'
   end
 end
