8585b36ccdc233c007c8333a2caaf00842e2c80b
Piotr Pasciak <ppasciak@pgs-soft.com>
2015-10-29 11:42:22 +0100

    block ability to send summary email while there is no summaries yet

--- app/wokers/hard_worker.rb
+++ app/wokers/hard_worker.rb
@@ -3,6 +3,6 @@ class HardWorker
 
   def perform(room, email)
     summaries = PokerRoom.find(room).summaries.order(created_at: :desc).limit(100).reverse
-    UserMailer.summaries_list_notification(email, room, summaries).deliver_later
+    UserMailer.summaries_list_notification(email, room, summaries).deliver_later if summaries.any?
   end
 end
